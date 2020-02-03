e2a688820eda1b805fd61b24ccdaaccadb3da733
pauzabalgoitia <40361509+pauzabalgoitia@users.noreply.github.com>
2020-02-03 16:48:03 -0600

    Add files via upload

---
+++ area rectangulo.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int A,H,B;
+	printf("ingrese la altura e ");
+	printf("ingrese la base");
+	scanf ("%d %d",&H,&B);
+	A=H*B;
+	printf("area= %d",A);
+	getche();
+}
---
+++ suma 2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int sum,A,B,C;
+	printf("ingresa 2 numeros enteros");
+	scanf("%d %d",&A,&B);
+	sum=A+B;
+	printf("suma= %d", sum);
+	getche();
+	
+}
