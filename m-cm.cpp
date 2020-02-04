5038ebd3b21b500eb7f9826629b6420723ec6940
pauzabalgoitia <40361509+pauzabalgoitia@users.noreply.github.com>
2020-02-03 20:13:06 -0600

    Add files via upload

---
+++ m-cm.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	float c;
+	printf("ingrese los centimetros");
+	scanf("%f", &c);
+	printf("%.2f metros",c * 100);
+	getche();
+	
+}
---
+++ promedio.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+ main()
+{
+	float a,b,c;
+	printf("ingresa la primera calificacion e ");
+	printf("ingresa la segunda calificacion");
+	scanf("%f %f",&a,&b);
+	c=a+b;
+	printf( "el promedio es de %.2f",c);
+    getche();
+	
+}
