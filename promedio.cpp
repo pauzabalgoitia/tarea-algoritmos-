7cf2660276eb526173435bd311c94de2feee8bfe
pauzabalgoitia <40361509+pauzabalgoitia@users.noreply.github.com>
2020-02-03 20:14:42 -0600

    Add files via upload

--- promedio.cpp
+++ promedio.cpp
@@ -6,7 +6,7 @@
 	printf("ingresa la primera calificacion e ");
 	printf("ingresa la segunda calificacion");
 	scanf("%f %f",&a,&b);
-	c=a+b;
+	c=(a+b)/2;
 	printf( "el promedio es de %.2f",c);
     getche();
 	
